f0fe1f4f7d5b5822d727a9726425136fca5fbf9d
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-10-18 17:19:04 +0800

    Handle diagonal from top right win condition

--- lib/board.rb
+++ lib/board.rb
@@ -10,8 +10,8 @@ class Board
   def initialize(board)
     @board = board
     @pattern = {
-      X: { row: [], column: [], diagonal: 0 },
-      Y: { row: [], column: [], diagonal: 0 }
+      X: { row: [], column: [], diagonal_left: 0, diagonal_right: 0 },
+      Y: { row: [], column: [], diagonal_left: 0, diagonal_right: 0 }
     }
   end
 
@@ -35,20 +35,42 @@ class Board
 
   def save_pattern(symbol, number)
     index = get_index(number)
+
+    # for vertical or horizontal
     @pattern[symbol][:row].push(index[:row])
     @pattern[symbol][:column].push(index[:column])
-    @pattern[symbol][:diagonal] += 1 if index[:row] == index[:column]
+
+    # for digonal from top left
+    @pattern[symbol][:diagonal_left] += 1 if index[:row] == index[:column]
+
+    # for diagonal from top right
+    save_diagonal_right(symbol, index)
   end
 
   def match?(symbol)
     row_tally = @pattern[symbol][:row].tally
     column_tally = @pattern[symbol][:column].tally
 
-    straight_line?(row_tally, column_tally) || @pattern[symbol][:diagonal] == 3
+    straight_line?(row_tally, column_tally) || diagonal?(symbol)
   end
 
   def straight_line?(row_tally, column_tally)
     vertical_horizontal?(row_tally) || vertical_horizontal?(column_tally)
+  end
+
+  def diagonal?(symbol)
+    @pattern[symbol][:diagonal_left] == 3 ||
+      @pattern[symbol][:diagonal_right] == 3
+  end
+
+  def save_diagonal_right(symbol, index)
+    row = index[:row]
+    column = index[:column]
+    return unless row.zero? && column == 2 ||
+                  row == 1 && column == 1 ||
+                  row == 2 && column.zero?
+
+    @pattern[symbol][:diagonal_right] += 1
   end
 
   def vertical_horizontal?(tally)
